d890ce659f443e2b76f06ad09cff233d8c5e0ab3
Victor <vic.toad.tor@free.fr>
2015-09-01 11:43:31 +0000

    NEW adventure template

--- bottle_app.py
+++ bottle_app.py
@@ -1,6 +1,10 @@
 # coding: utf-8
 # A very simple Bottle Hello World app for you to get started with...
 from bottle import default_app, route, static_file, view, post, request
+import datetime
+
+LONG_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+SHORT_TIME_FORMAT = '%Y-%m-%d'
 
 @route('/')
 def hello_world():
@@ -21,6 +25,12 @@ def maddam_submit():
     caption = request.forms.get('caption')
     image = request.files.get('upload')
     return 'Bravo ! Vous avez aidé mad-dam à faire son coup !'
+
+@view('an_adventure')
+def get_adventure_config(image_name, title, caption=None, date=None):
+    date = date or datetime.datetime.now()
+    caption = caption or ''
+    return dict(image_name=image_name, title=title, caption=caption, date=date.strftime(LONG_TIME_FORMAT))
 
 application = default_app()
 
---
+++ views/an_adventure.tpl
@@ -0,0 +1,8 @@
+---
+layout: default
+categories: adventure
+date:  {{ date }} 
+caption_header:  {{ title }}
+caption: {{ caption }}
+photo: {{ image_name }}
+---
