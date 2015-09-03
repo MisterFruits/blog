f2fdb06468c39c77832be61927b773af78ff7322
Victor <vic.toad.tor@free.fr>
2015-09-03 03:17:05 +0200

    CLEAN style

--- bottle_app.py
+++ bottle_app.py
@@ -9,21 +9,25 @@ SHORT_TIME_FORMAT = '%Y-%m-%d'
 ACCEPTED_EXT = (".jpg", '.jpeg', ".png")
 ADVENTURE_UPLOAD_PATH = "upload/mad-dam/"
 
-def is_regular_adventure(title, file):
-    if title is None or file is None:
+
+def is_regular_adventure(title, image):
+    if title is None or image is None:
         return False
-    _, file_ext = os.path.splitext(file.filename)
-    return file_ext.lower() in ACCEPTED_EXT
+    _, image_ext = os.path.splitext(image.filename)
+    return image_ext.lower() in ACCEPTED_EXT
 
 
 @route('/static/<filepath:path>')
 def serve_static(filepath):
-    return static_file(filepath, root = '/home/maxiimou/prod/pythonanywhere/static')
+    return static_file(filepath,
+                       root='/home/maxiimou/prod/pythonanywhere/static')
+
 
 @route('/mad-dam')
 @view('mad-dam')
 def maddam_input():
     return dict()
+
 
 @route('/mad-dam/m')
 @view('mad-dam')
@@ -42,23 +46,27 @@ def maddam_submit():
 
     date = datetime.datetime.now()
     adventure_name, image_ext = os.path.splitext(image.filename)
-    adventure_name = '-'.join((date.strftime(SHORT_TIME_FORMAT), adventure_name))
-    adventure_dir =ADVENTURE_UPLOAD_PATH + adventure_name + '/'
+    adventure_name = '-'.join((date.strftime(SHORT_TIME_FORMAT),
+                               adventure_name))
+    adventure_dir = ADVENTURE_UPLOAD_PATH + adventure_name + '/'
     caption = request.forms.get('caption')
     if os.path.exists(adventure_dir):
         return dict(success=False)
 
     os.makedirs(adventure_dir)
-    image.save(adventure_dir + adventure_name + image_ext)
-    with open(adventure_dir + adventure_name + '.markdown', 'w') as adventure_post:
-        adventure_post.write(get_adventure_config(adventure_name + image_ext, title, date, caption))
+    adventure_base = adventure_dir + adventure_name
+    image.save(adventure_base + image_ext)
+    with open(adventure_base + '.markdown', 'w') as adventure_post:
+        adventure_post.write(get_adventure_config(adventure_name + image_ext,
+                                                  title, date, caption))
 
     return dict(success=True)
+
 
 @view('an_adventure')
 def get_adventure_config(image_name, title, date, caption=None):
     caption = caption or ''
-    return dict(image_name=image_name, title=title, caption=caption, date=date.strftime(LONG_TIME_FORMAT))
+    return dict(image_name=image_name, title=title, caption=caption,
+                date=date.strftime(LONG_TIME_FORMAT))
 
 application = default_app()
-
